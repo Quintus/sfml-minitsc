304c81d9cba864d7ed9b93a0079f1aec4b32456b
Quintus <quintus@quintilianus.eu>
2015-03-21 18:52:41 +0100

    Add g

--- src/app.hpp
+++ src/app.hpp
@@ -13,12 +13,16 @@ public:
   inline sf::RenderWindow& get_renderwindow(){return *mp_renderwindow;}
   inline SceneManager& get_scene_manager(){return *mp_scene_manager;}
   inline TextureManager& get_texture_manager(){return *mp_texture_manager;}
+
+  inline float get_gravity_factor(){return m_gravity_factor;}
 private:
   sf::RenderWindow* mp_renderwindow;
   sf::Font m_gamefont;
 
   TextureManager* mp_texture_manager;
   SceneManager* mp_scene_manager;
+
+  float m_gravity_factor = 9.81f;
 };
 
 extern App* gp_app;
